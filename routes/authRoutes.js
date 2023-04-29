969d8ee19dc5cd86cf608fe1238365e0a56225f4
mythsSIMOU <126518752+mythsSIMOU@users.noreply.github.com>
2023-04-29 20:29:25 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
-const router = require ('express');
+const router = require("express");
 
-const authController = require('../Controllers/authController');
+const authController = require("../controllers/authController");
 
-const route = router( ) // instatiation
+const route = router();
 
 //route.get('/signup',authController.signup_get);
-route.post('/signup',authController.signup_post);
-route.get('/logout',authController.logout_get);
-route.post('/login',authController.login_post);
+route.post("/signup", authController.signup_post);
+route.get("/logout", authController.logout_get);
+route.post("/login", authController.login_post);
 
-module.exports = route ;
+module.exports = route;

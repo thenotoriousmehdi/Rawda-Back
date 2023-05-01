7b6117fcabc70db9e45c71af9a56e268da4f3d08
mythsSIMOU <126518752+mythsSIMOU@users.noreply.github.com>
2023-05-01 11:15:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log("Connected to app db & listening on port", process.env.PORT);
+      console.log("Connected to rawda db & listening on port", process.env.PORT);
     });
   })
   .catch((error) => {

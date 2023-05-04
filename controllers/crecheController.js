75ef48495442cc7825f6c41ff7ca8954e2962420
rxgofast <129177416+rxgofast@users.noreply.github.com>
2023-05-04 12:30:59 +0100

    Update crecheController.js

--- controllers/crecheController.js
+++ controllers/crecheController.js
@@ -1,65 +1,83 @@
 const Creche = require("../models/crecheModel");
 const mongoose = require("mongoose"); //pour ObjectId.isValid
+const users = require('../Models/UserModel');
+const proprio = require('../Models/proprioModel');
+const { LocalStorage } = require('node-localstorage');
+// ERROR HANDLER
+const localStorage = new LocalStorage('./localStorage');
+
 
 //Create new creche
 const ajouterCreche = async (req, res) => {
-  console.log(req.body);
-  const {
-    nom,
-    localisation,
-    typeAccueil,
-    joursAccueil,
-    typeEtab,
-    ageAccueil,
-    pedagogie,
-    langue,
-    capacite,
-    transport,
-    alimentation,
-    num,
-    mail,
-    description,
-    prop,
-  } = req.body;
- 
+  const userMail = localStorage.getItem('key');
+  const key={};
+  key.email = userMail;
+  const user= await users.findOne(key);
+  if (user){
+    const id=user._id ;
+    pro = proprio.find({ userID: id })
+    .populate('userID')
+    .populate('creche');
+    
+    console.log(pro);
+    if (pro){
+          console.log(req.body);
+          const {
+            nom,
+            localisation,
+            typeAccueil,
+            joursAccueil,
+            typeEtab,
+            ageAccueil,
+            pedagogie,
+            langue,
+            capacite,
+            transport,
+            alimentation,
+            num,
+            mail,
+            description,
+            prop,
+          } = req.body;
+        
 
-  //Ajouter la creche a la BD
-  const photosPaths = [];
-  if (req.files) {
-    let files = req.files;
-    for (let i = 0; i < files.length; i++) {
-      photosPaths.push(files[i].filename);
-    }
-  }
-  try {
-    const creche = await Creche.create({
-      nom,
-      localisation,
-      typeAccueil,
-      joursAccueil,
-      typeEtab,
-      ageAccueil:{
-         ageMin: ageAccueil,
-         ageMax: undefined
-      },
-      pedagogie,
-      langue,
-      capacite,
-      transport,
-      alimentation,
-      num,
-      mail,
-      description,
-      prop,
-      photos: photosPaths.map((path) => `uploads/${path}`),
-    });
-    console.log('creche');
-    res.status(200).json(creche);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json(error);
-  }
-};
+          //Ajouter la creche a la BD
+          const photosPaths = [];
+          if (req.files) {
+            let files = req.files;
+            for (let i = 0; i < files.length; i++) {
+              photosPaths.push(files[i].filename);
+            }
+          }
+          try {
+            const creche = await Creche.create({
+              nom,
+              localisation,
+              typeAccueil,
+              joursAccueil,
+              typeEtab,
+              ageAccueil:{
+                ageMin: ageAccueil,
+                ageMax: undefined
+              },
+              pedagogie,
+              langue,
+              capacite,
+              transport,
+              alimentation,
+              num,
+              mail,
+              description,
+              prop,
+              photos: photosPaths.map((path) => `uploads/${path}`),
+            });
+            console.log('creche');
+            res.status(200).json(creche);
+          } catch (error) {
+            console.log(error);
+            res.status(400).json(error);
+          }}else{res.json({error:' NOT ALLOWED'})};
+}else { res.json({ error: 'user not found'})}};
 
 const getCreches = async (req, res) => {
   const creche = await Creche.find({});

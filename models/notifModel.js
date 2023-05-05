1ebf54168a70c4830c6b66ef2f3f89dd84b9c4f8
Mouloud08 <84584439+Mouloud08@users.noreply.github.com>
2023-05-05 01:33:28 +0100

    Update notifModel.js

--- models/notifModel.js
+++ models/notifModel.js
@@ -1,15 +1,46 @@
-const mongoose = require("mongoose");
+onst mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const notifSchema = new Schema({
-    nom: { type: String, required: true },
-    prenom: { type: String, reqiured: true },
-    dateRdv: { type: String, required: true },
-    heureRdv: { type: String, required: true },
-    email: { type: String, required: true },
-    numeroTelephone: { type: String, required: true}
+    nomParent: {
+        type: String,
+        required: false,
     },
-    {timestamps : true, });
-
-module.exports = mongoose.model("notifications", notifSchema);
+    prenomParent: {
+        type: String,
+        required: false,
+    },
+    nomEnfant: {
+        type: String,
+        required: false,
+    },
+    prenomEnfant: {
+        type: String,
+        required: false,
+    },
+    dateNaissance: {
+        type: String,
+        required: false,
+    },
+    dateEntree: {
+        type: String,
+        required: false
+    },
+    heure: {
+        type: String,
+        required: false
+    },
+    dateRdv: {
+        type: String,
+        required: false
+    },
+    proprietaire: {
+        type: Schema.Types.ObjectId,
+        ref: "proprios",
+        required: true
+    }
+}, {
+    timestamps: true
+})
+module.exports = mongoose.model("notif", notifSchema);

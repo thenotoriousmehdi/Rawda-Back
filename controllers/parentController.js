a9ca993a86db0b91689feab2f9866c3b6ef668da
rxgofast <129177416+rxgofast@users.noreply.github.com>
2023-05-05 20:08:02 +0100

    Update parentController.js

--- controllers/parentController.js
+++ controllers/parentController.js
@@ -1,60 +1,52 @@
 const mongoose = require("mongoose");
+const  {ObjectId} = require("mongodb") ;
 const Parent = require('../models/parentModel');
 const Enfant = require('../models/enfantModel');
+const users = require('../Models/UserModel');
+const { LocalStorage } = require('node-localstorage');
+// ERROR HANDLER
+const localStorage = new LocalStorage('./localStorage');
 
-exports.addEnfantToParent = async(req, res) => {
-    const { parentID, nom, prenom, date_naissance, creche } = req.body;
-
-    try {
-        // Find the parent by ID
-        const parent = await Parent.findById(parentID);
-
-        if (!parent) {
-            return res.status(404).json({ msg: 'Parent not found' });
+    exports.addEnfantToParent = async(req, res) => {
+        const enfants={};
+        enfants.nom=req.body.nom;
+        enfants.prenom=req.body.prenom;
+        enfants.date_naissance=req.body.date_naissance;
+        enfants.creche= new ObjectId(req.params.id);;
+        const userMail = localStorage.getItem('key');  
+        console.log(userMail);
+        const user = await users.findOne({email : userMail});
+        console.log(user._id);
+        if (user){
+        try {
+            const parentID = user._id;
+            console.log(parentID);
+            // Find the parent by ID
+            parent =  await Parent.findOne({ userID: parentID });
+            console.log(parent);
+            if (!parent) {
+                return res.status(404).json({ msg: 'Parent not found' });
+            }
+            // Create a new enfants
+            const enfant = new Enfant(enfants);
+        console.log(enfant);
+            // Save the enfant in the database
+        
+            await enfant.save();
+            parent.enfant=[];
+            // Add the enfant to the parent's enfant array
+            parent.enfant.push(enfant);
+        
+            // Save the parent with the new enfant
+            parent.save();
+            res.json(parent);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
         }
-
-        // Create a new enfant
-        const enfant = new Enfant({
-            nom,
-            prenom,
-            date_naissance,
-            creche
-        });
-
-        // Save the enfant in the database
-        await enfant.save();
-
-        // Add the enfant to the parent's enfant array
-        parent.enfant.push(enfant);
-
-        // Save the parent with the new enfant
-        await parent.save();
-
-        res.json(parent);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+    }else{ res.status(404).json({error:' YOU DONT HAVE THE ACCESS'})};
     }
-};
 
-
-exports.ajouterParent = async(req, res) => {
-    try {
-        const { userID, enfant } = req.body;
-
-        const parent = new Parent({
-            userID,
-            enfant
-        });
-
-        await parent.save();
-
-        res.status(201).json({ message: "Le parent a été ajouté avec succès" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Une erreur est survenue lors de l'ajout du parent" });
-    }
-};
 
 
 exports.obtenirTousLesParents = async(req, res) => {

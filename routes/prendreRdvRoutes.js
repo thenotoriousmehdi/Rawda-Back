c726ae4ca0734dc6e190ea459132e89b93ce51cd
Mouloud08 <84584439+Mouloud08@users.noreply.github.com>
2023-05-06 21:28:37 +0100

    Create prendreRdvRoutes.js

---
+++ routes/prendreRdvRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const Notif = require('../models/notifModel');
+const Creche = require('../models/crecheModel');
+const Enfant = require('../models/enfantModel');
+const Proprio = require('../models/proprioModel');
+const User = require('../models/userModel');
+const Parent = require("../models/parentModel");
+
+const { reserverPlace, prendreRendezVous } = require("../controllers/prendreRdvController");
+
+
+const router = express.Router();
+
+router.post('/crechereserver/:id', reserverPlace);
+router.post('/crecherdv/:id', prendreRendezVous);
+
+module.exports = router;

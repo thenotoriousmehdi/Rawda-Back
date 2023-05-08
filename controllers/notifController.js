6bf885cdc3313b084ee2b69c44d1815be972301d
Mouloud08 <84584439+Mouloud08@users.noreply.github.com>
2023-05-08 11:41:46 +0100

    Update notifController.js

--- controllers/notifController.js
+++ controllers/notifController.js
@@ -1,11 +1,37 @@
 const mongoose = require("mongoose");
 const Notif = require("../models/notifModel");
+const Proprio = require("../models/proprioModel");
+const { ObjectId } = require("mongodb");
+const User = require("../models/userModel");
 
 // Récupérer les notifications propriétaire pour un propriétaire donné
 const obtenirNotifsPropParProp = async(req, res) => {
-    const propId = req.params.propId; // récupérer l'ID du propriétaire à partir des paramètres de requête
-    let notifs;
-    notifs = await Notif.find({ proprietaire: propId });
+    //const userMail = localStorage.getItem('key');
+    const userId = new ObjectId(req.params.id);
+    const user = await User.findById(userId);
+    if (!user) {
+        res.send("User not found");
+        return;
+    } else {
+        res.send("User found");
+    }
+
+
+    const proprietaire = await Proprio.findOne({ userID: user._id });
+    if (!proprietaire) {
+        res.send("proprio not found");
+        return;
+    } else {
+        res.send("proprio found");
+    }
+    // récupérer l'ID du propriétaire à partir des paramètres de requête
+    const notifs = proprietaire.notification;
+    if (!notifs) {
+        res.send("notifs not found");
+        return;
+    } else {
+        res.send("notifs found");
+    }
     res.status(201).json(notifs);
 };
 //ramener l'id de la creche courante, ensuite ramener l'id du proprietaire

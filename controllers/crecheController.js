92cdb8ce4d0789a152bfa12ac11ecf4cd599614d
mythsSIMOU <126518752+mythsSIMOU@users.noreply.github.com>
2023-05-08 19:08:18 +0100

    Update crecheController.js

--- controllers/crecheController.js
+++ controllers/crecheController.js
@@ -181,7 +181,7 @@ const modifyCreche = async (req, res) => {
 const home = (req, res) => {
   // Récupérer les 7 crèches les mieux notées
   Creche.find()
-    .sort({ "avis.note":- 1 })
+    .sort({ "avis.note": -1 })
     .limit(7)
     .then((creches) => {
       //console.log("7 crèches les mieux notées :", creches);
@@ -224,12 +224,12 @@ const evaluerCreche = async (req, res) => {
   const coms = creche.avis.evaluations;
 
   const newEval = {
-    noteIndiv:newNote,
+    noteIndiv: newNote,
     nom: nom,
     commentaires: req.body.commentaires,
   };
-  coms.push(newEval);
   newNote = (creche.avis.note * coms.length + newNote) / (coms.length + 1);
+  coms.push(newEval);
 
   const update = await Creche.findOneAndUpdate(
     { _id: id },

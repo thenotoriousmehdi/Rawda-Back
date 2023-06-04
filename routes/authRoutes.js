791e8ef55bc3b4386bb02b8bf756ec6a4e65005e
rxgofast <129177416+rxgofast@users.noreply.github.com>
2023-06-04 14:58:26 +0100

    Update authRoutes.js
    J'ai ajouter les middlewear d'acces

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const authController = require("../controllers/authController");
+const ac = require("../middleware/accessControl");
+const { isAuthentificated } = require("../middleware/auth");
+const authController = require("../controllers/authController")
 
 const route = express.Router();
 
-//route.get('/signup',authController.signup_get);
-route.post("/signup", authController.signup_post);
-route.get("/logout", authController.logout_get);
+route.post("/signup", authController.signup_post );
 route.post("/login", authController.login_post);
 route.post("/documents", authController.documents_post);
-route.get("/profile", authController.get_profile);
-route.patch("/modifierProfile", authController.modifInfoProfile);
+route.get("/profile", ac.lireProfile , isAuthentificated, authController.get_profile);
+route.patch("/modifierProfile",isAuthentificated,ac.modifierUser, authController.modifInfoProfile);
 
 module.exports = route;

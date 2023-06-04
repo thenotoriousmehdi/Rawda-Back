09c52ac05d64bbdc84c2c7d6f206b159309d86f0
mythsSIMOU <lw_beldjoudi@esi.dz>
2023-06-04 21:06:08 +0100

    Majuscule hh

--- routes/prendreRdvRoutes.js
+++ routes/prendreRdvRoutes.js
@@ -3,7 +3,7 @@ const Notif = require('../models/notifModel');
 const Creche = require('../models/crecheModel');
 const Enfant = require('../models/enfantModel');
 const Proprio = require('../models/proprioModel');
-const User = require('../Models/UserModel');
+const User = require('../models/userModel');
 const Parent = require("../models/parentModel");
 const {isAuthentificated} = require('../middleware/auth');
 

f00aed4a955c90dd9d4250fce136702c381179d8
rxgofast <129177416+rxgofast@users.noreply.github.com>
2023-06-04 14:59:02 +0100

    Update Creche.js

--- routes/Creche.js
+++ routes/Creche.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const upload = require("../middleware/upload");
+const { isAuthentificated} = require("../middleware/auth");
+const ac = require("../middleware/accessControl");
 
 const {
   ajouterCreche,
@@ -13,10 +15,10 @@ const {
 
 const router = express.Router();
 
-router.get("/Creche", getCreches);
+router.get("/Creche", isAuthentificated ,ac.lireCrechePermission,getCreches);
 
 router.post(
-  "/Creche",
+  "/Creche",isAuthentificated,
   upload.fields([
     { name: "photos", maxCount: 10 },
     { name: "carteNationale", maxCount: 1 },
@@ -25,14 +27,14 @@ router.post(
   ajouterCreche
 );
 
-router.delete("/Creche/:id", deleteCreche);
+router.delete("/Creche/:id", isAuthentificated,ac.SupprimerCreche, deleteCreche);
 
-router.patch("/Creche/:id", modifyCreche);
+router.patch("/Creche/:id",isAuthentificated ,ac.modifierCrechePermission, modifyCreche);
 
-router.get("/Creche/:id", infoCreche);
+router.get("/Creche/:id",ac.lireCrechePermission ,infoCreche);
 
 router.get("/Home", home);
 
-router.patch("/evaluerCreche/:id", evaluerCreche);
+router.patch("/evaluerCreche/:id", isAuthentificated ,evaluerCreche);
 
 module.exports = router;

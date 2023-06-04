e059f6a53f32dac1b08d1f5ae8c6f8a770569f66
rxgofast <129177416+rxgofast@users.noreply.github.com>
2023-06-04 15:00:56 +0100

    Update prendreRdvRoutes.js

--- routes/prendreRdvRoutes.js
+++ routes/prendreRdvRoutes.js
@@ -3,15 +3,16 @@ const Notif = require('../models/notifModel');
 const Creche = require('../models/crecheModel');
 const Enfant = require('../models/enfantModel');
 const Proprio = require('../models/proprioModel');
-const User = require('../models/userModel');
+const User = require('../Models/UserModel');
 const Parent = require("../models/parentModel");
-
+const {isAuthentificated} = require('../middleware/auth');
 
 const { reserverPlace, prendreRendezVous } = require("../controllers/prendreRdvController");
 
 
 const router = express.Router();
 
-router.post('/crechereserver/:id', reserverPlace);
-router.post('/crecherdv/', prendreRendezVous);
+router.post('/crechereserver/:id',isAuthentificated, reserverPlace);
+router.post('/crecherdv/:id', isAuthentificated,prendreRendezVous);
+
 module.exports = router;

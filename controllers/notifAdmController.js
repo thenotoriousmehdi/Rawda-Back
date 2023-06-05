de0eff08e8b4f2f26edc4c51f84ccf0c3b52af9f
Mouloud08 <84584439+Mouloud08@users.noreply.github.com>
2023-06-06 00:24:28 +0100

    Update notifAdmController.js

--- controllers/notifAdmController.js
+++ controllers/notifAdmController.js
@@ -1,26 +1,34 @@
 const mongoose = require("mongoose");
 const NotifAdm = require("../models/notifAdmModel");
 const User = require("../models/userModel");
+const { LocalStorage } = require('node-localstorage');
+const localStorage = new LocalStorage("./localStorage");
 
 
 // Récupérer les notifications propriétaire pour un propriétaire donné
 const obtenirNotifsAdm = async(req, res) => {
     // récupérer l'ID du propriétaire à partir des paramètres de requête
-    let notifsAdmin;
-    notifsAdmin = await NotifAdm.find().sort({ timestamps: -1 });
+    const notifsAdmin = await NotifAdm.find().sort({ timestamps: -1 });
     res.status(201).json(notifsAdmin);
     // didnt handle errors yet
 };
 const ajouterNotifAdm = async(req, res) => {
     //didnt handle errors yet
-    const { nomProprio, prenomProprio, creche } = req.body;
-    let notifAdm;
-
-    notifAdm = await new NotifAdm({
+    const { creche } = req.body;
+    const userMail = localStorage.getItem("key");
+    const key = {};
+    key.email = userMail;
+    const user = await User.findOne(key);
+    const nomProprio = user.nom;
+    const prenomProprio = user.prenom;
+    const emailProprio = user.email;
+    notifAdm = new NotifAdm({
         nomProprio,
         prenomProprio,
-        creche
+        creche: creche,
+        emailProprio,
     });
+    console.log(notifAdm);
 
     await notifAdm.save(notifAdm);
 

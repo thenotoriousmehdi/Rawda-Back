a27b25c9a5e7e4350adb2ec1b273f32f88cc5bac
rxgofast <129177416+rxgofast@users.noreply.github.com>
2023-06-05 23:45:11 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const ac = require("../middleware/accessControl");
-const upload = require("../middleware/upload");
 const { isAuthentificated } = require("../middleware/auth");
-const authController = require("../controllers/authController");
-
+const authController = require("../controllers/authController")
+const upload = require("../middleware/upload");
 const route = express.Router();
 
-route.post("/signup", authController.signup_post);
+route.post("/signup", authController.signup_post );
 route.post("/login", authController.login_post);
 route.post("/documents", authController.documents_post);
-route.get(
-  "/profile",
-  ac.lireProfile,
-  isAuthentificated,
-  authController.get_profile
-);
+route.get("/profile", ac.lireProfile , isAuthentificated, authController.get_profile);
 route.patch(
-  "/modifierProfile",
-  isAuthentificated,
-  ac.modifierUser,
-  upload.fields([{ name: "photo", maxCount: 1 }]),
-  authController.modifInfoProfile
-);
+    "/modifierProfile",
+    isAuthentificated,
+    ac.modifierUser,
+    upload.fields([{ name: "photo", maxCount: 1 }]),
+    authController.modifInfoProfile
+  );
+route.post("/logout",authController.logout);
 
 module.exports = route;

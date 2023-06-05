8d583f599a569477712e3566a46d4bb6c3ad2cd6
Mouloud08 <84584439+Mouloud08@users.noreply.github.com>
2023-06-05 08:43:35 +0100

    Update prendreRdvRoutes.js

--- routes/prendreRdvRoutes.js
+++ routes/prendreRdvRoutes.js
@@ -5,14 +5,12 @@ const Enfant = require('../models/enfantModel');
 const Proprio = require('../models/proprioModel');
 const User = require('../models/userModel');
 const Parent = require("../models/parentModel");
-const {isAuthentificated} = require('../middleware/auth');
 
 const { reserverPlace, prendreRendezVous } = require("../controllers/prendreRdvController");
 
 
 const router = express.Router();
 
-router.post('/crechereserver/:id',isAuthentificated, reserverPlace);
-router.post('/crecherdv/:id', isAuthentificated,prendreRendezVous);
-
+router.post('/crechereserver/:crecheId', reserverPlace);
+router.post('/crecherdv/:crecheId', prendreRendezVous);
 module.exports = router;
